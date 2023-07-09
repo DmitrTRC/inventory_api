d40e8ff2bfc57385a2e8ee802434f46e73c676a5
Dmitry Morozov <9984398@gmail.com>
2023-07-09 20:36:07 +0300

    -> Base project structure: Done

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,8 @@ from api.spectacular.urls import urlpatterns as doc_urls
 app_name = 'api'
 
 urlpatterns = [
-    # path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls.jwt')),
+
 ]
 
 urlpatterns += doc_urls
