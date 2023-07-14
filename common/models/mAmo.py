455f039b9dcc33b1187c98c7bd67c43a271958bb
Dmitry Morozov <9984398@gmail.com>
2023-07-14 21:47:10 +0300

    --> Migrations: Done

--- common/models/mAmo.py
+++ common/models/mAmo.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from common.models.mCaliber import Caliber
+
 
 class Amo(models.Model):
     type = models.ForeignKey('AmoType', on_delete=models.RESTRICT, blank=False, null=True)
@@ -10,7 +12,7 @@ class AmoType(models.Model):
     name = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='amo_manufacture_photos', blank=True)
     country = models.CharField(max_length=100, blank=False, null=False, default='USA')
-    caliber = models.ForeignKey('Caliber', on_delete=models.RESTRICT, blank=False, null=True, related_name='calibers')
+    caliber = models.ForeignKey(Caliber, on_delete=models.RESTRICT, blank=False, null=True)
     bullet_weight = models.CharField(max_length=100, blank=False, null=False, default='55gr')
     bullet_type = models.CharField(max_length=100, blank=False, null=False, default='FMJ')
     ballistics_sheet = models.FileField(upload_to='ballistics_sheets', blank=True)
--- common/models/mWeapon.py
+++ common/models/mWeapon.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from common.models.mCaliber import Caliber
+
 
 class WeaponItem(models.Model):
     item_model = models.ForeignKey('WeaponModel', on_delete=models.RESTRICT, blank=False, null=False)
@@ -17,10 +19,11 @@ class WeaponItem(models.Model):
         return self.item_model.name
 
 
+# TODO: Refactor to lazy load
 class WeaponModel(models.Model):
     name = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='weapon_item_photos', blank=True)
-    caliber = models.ForeignKey('Caliber', on_delete=models.RESTRICT, blank=False, null=True)
+    caliber = models.ForeignKey(Caliber, on_delete=models.RESTRICT, blank=False, null=True)
     manufacturer = models.ForeignKey('WeaponManufacturer', on_delete=models.RESTRICT, blank=False, null=True)
 
     def __str__(self):
@@ -28,7 +31,7 @@ class WeaponModel(models.Model):
 
 
 class WeaponManufacturer(models.Model):
-    name = models.CharField(max_length=200)
+    brand_name = models.CharField(max_length=200)
     country = models.CharField(max_length=100, blank=False, null=False, default='USA')
     website = models.CharField(max_length=100, blank=False, null=False, default='https://www.google.com')
 
